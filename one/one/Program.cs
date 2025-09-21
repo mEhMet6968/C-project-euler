fb18b64c1c76bf72face07ee89cfb175b6005a1b
Mehmet Emin <73130601+mEhMet6968@users.noreply.github.com>
2025-09-21 19:58:18 +0300

    first problem

---
+++ one/one/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		int limit = 1000;
+		int sum = 0;
+		for (int i = 1; i < limit; i++)
+		{
+			if (i % 3 == 0 || i % 5 == 0)
+				sum += i;
+		}
+		Console.WriteLine(sum); // 233168
+	}
+}
