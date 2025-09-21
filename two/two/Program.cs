7764a6eb5563960e0b07e5e7b07c65eaa4ccc68c
Mehmet Emin <73130601+mEhMet6968@users.noreply.github.com>
2025-09-21 20:00:12 +0300

    second problem

---
+++ two/two/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		int limit = 4000000;
+		int a = 1, b = 2;
+		int sum = 0;
+
+		while (b <= limit)
+		{
+			if (b % 2 == 0)
+				sum += b;
+
+			int temp = a + b;
+			a = b;
+			b = temp;
+		}
+
+		Console.WriteLine(sum); // 4613732
+	}
+}
