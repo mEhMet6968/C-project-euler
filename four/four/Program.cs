cd8088c67bab390a9628b7b4bf6e6249d810ade1
Mehmet Emin <73130601+mEhMet6968@users.noreply.github.com>
2025-09-23 19:36:35 +0300

    problem four

---
+++ four/four/Program.cs
@@ -0,0 +1,26 @@
+﻿int number;
+List<int> pal = new List<int>() { };
+for (int i = 999; i > 100; i--)
+{
+	for (int j = 999; j > 100; j--)
+	{
+		number = i * j;
+		string metin = number.ToString();
+		char[] karakterler = metin.ToCharArray();
+		Array.Reverse(karakterler);
+		string tersMetin = new string(karakterler);
+		if (metin == tersMetin)
+		{
+			pal.Add(number);
+		}
+		int enBuyuk = int.MinValue;
+		foreach (int sayi in pal)
+		{
+			if (sayi > enBuyuk)
+			{
+				enBuyuk = sayi;
+			}
+		}
+		Console.WriteLine("En büyük sayı: " + enBuyuk);
+	}
+}
