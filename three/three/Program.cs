ca22ae54efd3f732e12eadbb7c6f1cea9419b8ad
Mehmet Emin <73130601+mEhMet6968@users.noreply.github.com>
2025-09-23 19:35:05 +0300

    problem three

---
+++ three/three/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		long number = 600851475143;
+		long factor = 2;
+		long largest = 0;
+
+		while (number > 1)
+		{
+			if (number % factor == 0)
+			{
+				largest = factor;
+				number /= factor;
+				while (number % factor == 0)
+					number /= factor;
+			}
+			factor++;
+		}
+
+		Console.WriteLine(largest); // 6857
+	}
+}
