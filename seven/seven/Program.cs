3fb2da6be394e16d11c809fee6b1b27d529e2df8
Mehmet Emin <73130601+mEhMet6968@users.noreply.github.com>
2025-09-25 19:54:25 +0300

    problem seven

---
+++ seven/seven/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		int count = 0;
+		int number = 1;
+
+		while (count < 10001)
+		{
+			number++;
+			if (asalmila(number))
+			{
+				count++;
+				Console.WriteLine("asal sayı FOUNDEDDDDDDD ");
+
+			}
+			Console.WriteLine("asal sayı: " + number);
+
+		}
+
+		Console.WriteLine("bittiiiiiiiiiiiiiiiiiiiiiiiiiiii" + number);
+	}
+
+	static bool asalmila(int n)
+	{
+		if (n < 2)
+			return false;
+
+		for (int i = 2; i <= Math.Sqrt(n); i++)
+		{
+			if (n % i == 0)
+				return false;
+		}
+
+		return true;
+
+	}
+}
