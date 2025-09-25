6ab6aa83f5a2ebe0921d99f477327a669239e099
Mehmet Emin <73130601+mEhMet6968@users.noreply.github.com>
2025-09-25 19:53:44 +0300

    problem six

---
+++ six/six/Program.cs
@@ -0,0 +1,37 @@
+﻿int toplam = 0;
+
+int makul = 0;
+
+int carpim = 1;
+
+List<int> number = new List<int>() { };
+
+for (int i = 1; i <= 100; i++)
+
+{
+
+	toplam = i + toplam;
+
+}
+
+for (int l = 0; l <= 100; l++)
+
+{
+
+	number.Add(l * l);
+
+}
+
+foreach (int sayi in number)
+
+{
+
+	makul += sayi;
+
+}
+
+Console.WriteLine(makul);
+
+Console.WriteLine(toplam * toplam);
+
+Console.WriteLine("İstenen Cevap " + ((toplam * toplam) - makul));
