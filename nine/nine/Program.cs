9c144a69d7fee311ea4adedcef00436f05437a77
Mehmet Emin <73130601+mEhMet6968@users.noreply.github.com>
2025-09-26 10:09:57 +0300

    problem nine

---
+++ nine/nine/Program.cs
@@ -0,0 +1,15 @@
+﻿int c = 1;
+for (int a = 1; a <= 332; a++)
+{
+	for (int b = a + 1; b < 600; b++)
+	{
+		c = 1000 - b - a;
+		if ((a * a) + (b * b) == (c * c))
+		{
+			Console.WriteLine("bitti abi " + a + " " + b + " " + c);
+		}
+
+	}
+
+}
+//have fun guys
