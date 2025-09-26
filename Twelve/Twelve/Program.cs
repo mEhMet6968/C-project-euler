8c420f08df88bc998a65508c1dc36eb390a9b2db
Mehmet Emin <73130601+mEhMet6968@users.noreply.github.com>
2025-09-27 00:24:44 +0300

    problem twelve

---
+++ Twelve/Twelve/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+long n = 10000;
+while (true)
+{
+	int sayac = 0;
+	long triangular = n * (n + 1) / 2;
+	Console.WriteLine($"T_{n} = {triangular}");
+	for (int i = 1; i < triangular; i++)
+	{
+		if (triangular % i == 0)
+		{
+			sayac++;
+		}
+		if (sayac > 500)
+		{
+			Console.WriteLine("Bulduk laaa" + triangular);
+			break;
+		}
+	}
+	n++;
+	sayac = 0;
+
+
+}
