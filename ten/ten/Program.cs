c4b79b69f21046d3bc3012960d305b7818ee114e
Mehmet Emin <73130601+mEhMet6968@users.noreply.github.com>
2025-09-26 10:10:14 +0300

    problem ten

---
+++ ten/ten/Program.cs
@@ -0,0 +1,26 @@
+﻿List<int> number = new List<int>();
+long valueeee = 0;
+
+for (int i = 2; i < 2000000; i++)
+{
+	bool isPrime = true;
+	for (int a = 2; a * a <= i; a++)
+	{
+		if (i % a == 0)
+		{
+			isPrime = false;
+			break;
+		}
+	}
+	if (isPrime)
+	{
+		number.Add(i);
+	}
+}
+
+foreach (int i in number)
+{
+	valueeee += i;
+}
+
+Console.WriteLine(valueeee);
