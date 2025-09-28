2b1bb032281cfa7dbee969ee8d023b587090fa80
Mehmet Emin <73130601+mEhMet6968@users.noreply.github.com>
2025-09-28 12:45:58 +0300

    project sixteen

---
+++ sixteen/sixteen/Program.cs
@@ -0,0 +1,17 @@
+﻿//this number so big for C# int value i must need Numerics lib
+
+using System.Numerics;
+
+BigInteger output = 1;
+BigInteger sum = 0;
+for (int i = 0; i < 1000; i++)
+{
+	output *= 2;
+}
+while (output > 0)
+{
+	BigInteger digit = output % 10;
+	sum += digit;
+	output /= 10;
+}
+Console.WriteLine(sum);
